49da77e70739e9b24653bad67e6444fc7f67e3ee
Le Anh Quan <quanla2003@gmail.com>
2015-06-07 18:06:26 +0700

    Support Json request url param

--- src/main/java/qj/tool/web/json/JsonAction.java
+++ src/main/java/qj/tool/web/json/JsonAction.java
@@ -2,10 +2,22 @@ package qj.tool.web.json;
 
 import qj.util.funct.F1;
 
+import java.util.List;
+
 public class JsonAction {
-        public String url;
-        public String method;
-        public Class<?>[] requestParams;
-        public Class<?> response;
-        public F1<Object[],Object> exec;
+    public JsonServlet.UrlPattern url;
+    public String method;
+    public List<RequestParam> requestParams;
+    public Class<?> response;
+    public F1<Object[], Object> exec;
+
+    public static class RequestParam {
+        Class<?> clazz;
+        String urlParamName;
+
+        public RequestParam(Class<?> clazz, String urlParamName) {
+            this.clazz = clazz;
+            this.urlParamName = urlParamName;
+        }
+    }
 }
--- src/main/java/qj/tool/web/json/JsonServlet.java
+++ src/main/java/qj/tool/web/json/JsonServlet.java
@@ -2,8 +2,10 @@ package qj.tool.web.json;
 
 
 import com.google.gson.Gson;
+import qj.util.Cols;
 import qj.util.LangUtil;
 import qj.util.ReflectUtil;
+import qj.util.RegexUtil;
 import qj.util.funct.*;
 
 import javax.servlet.ServletException;
@@ -13,11 +15,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Objects;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JsonServlet extends HttpServlet {
     LinkedList<JsonAction> actions = new LinkedList<>();
@@ -40,13 +42,15 @@ public class JsonServlet extends HttpServlet {
         String requestURI = req.getRequestURI();
         String method = req.getMethod();
         for (JsonAction action : actions) {
-            if (Objects.equals(action.url, requestURI) && Objects.equals(action.method, method)) {
+            Map<String,String> matchedUrlParams;
+            if (Objects.equals(action.method, method) && (matchedUrlParams = action.url.matchUrlParams(requestURI)) != null) {
                 Gson gson = new Gson();
 
                 LinkedList<P0> afterExec = new LinkedList<>();
                 ArrayList<Object> params = new ArrayList<>();
-                for (Class<?> requestParam : action.requestParams) {
-                    Douce<?,P0> param = createParam(req, gson, requestParam);
+                for (JsonAction.RequestParam requestParam : action.requestParams) {
+                    Douce<?,P0> param = requestParam.urlParamName == null ? createParam(req, gson, requestParam.clazz) :
+                            new Douce<>(toType(requestParam.clazz, matchedUrlParams.get(requestParam.urlParamName)), null);
                     if (param.get2()!=null) {
                         afterExec.add(param.get2());
                     }
@@ -66,6 +70,14 @@ public class JsonServlet extends HttpServlet {
             }
         }
         resp.sendError(404);
+    }
+
+    private <A> A toType(Class<A> clazz, String value) {
+        if (clazz == String.class) {
+            return (A) value;
+        } else {
+            return (A) Long.valueOf(value);
+        }
     }
 
     private Douce<?,P0> createParam(HttpServletRequest req, Gson gson, Class<?> requestParam) throws IOException {
@@ -109,12 +121,19 @@ public class JsonServlet extends HttpServlet {
 
     private static JsonAction createAction(Method m, Class clazz, String url, String method) {
         JsonAction jsonAction = new JsonAction();
-        jsonAction.url = url;
+        jsonAction.url = parseUrlPattern(url);
         jsonAction.method = method;
 
 
+        ArrayList<JsonAction.RequestParam> requestParams = new ArrayList<>();
+        for (int i = 0; i < m.getParameterTypes().length; i++) {
+            Class<?> paramType = m.getParameterTypes()[i];
+            Annotation[] annos = m.getParameterAnnotations()[i];
+            UrlParam urlParam = (UrlParam) Cols.find(annos, (a) -> a.annotationType() == UrlParam.class);
+            requestParams.add(new JsonAction.RequestParam(paramType, urlParam == null ? null : urlParam.value()));
+        }
+        jsonAction.requestParams = requestParams;
 
-        jsonAction.requestParams = m.getParameterTypes();
         jsonAction.response = m.getReturnType();
         jsonAction.exec = (params) -> {
             Object action = ReflectUtil.newInstance4(clazz);
@@ -122,6 +141,39 @@ public class JsonServlet extends HttpServlet {
         };
 
         return jsonAction;
+    }
+
+    static class UrlPattern {
+
+        public Pattern ptn;
+        public LinkedList<String> names;
+
+
+        private Map<String, String> matchUrlParams(String reqUrl) {
+            Matcher matcher = ptn.matcher(reqUrl);
+            if (!matcher.matches()) {
+                return null;
+            }
+
+            HashMap<String, String> ret = new HashMap<>();
+            for (int i = 0; i < names.size(); i++) {
+                String name = names.get(i);
+                ret.put(name, matcher.group(i+1));
+            }
+            return ret;
+        }
+
+    }
+
+    private static UrlPattern parseUrlPattern(String url) {
+        UrlPattern urlPattern = new UrlPattern();
+        LinkedList<String> names = new LinkedList<>();
+        urlPattern.ptn = Pattern.compile(RegexUtil.replaceAll(url, ":(\\w+)", (m) -> {
+            names.add(m.group(1));
+            return "([^/]+)";
+        }));
+        urlPattern.names = names;
+        return urlPattern;
     }
 
 }
